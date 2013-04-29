15b89e94484f3511e1fd68374a07045837fc0460
tushis <tushis@mail.ru>
2013-04-29 11:19:46 +0200

    29/4/2013 removed block from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 BiolabProject::Application.initialize!
 
-namespace :gems do
-  task :install do
-   run "cd #{}/current && RAILS_ENV=production rake gems:install"
-  end
-end
