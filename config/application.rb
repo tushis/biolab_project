5137ea60f637bcf8149dbc58f2e175cc83ed19ea
tushis <tushis@mail.ru>
2013-05-21 01:52:51 +0200

    config.assets.paths << Rails.root.join("app/sites/#{domain}/assets/#{dir}").to_s

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module BiolabProject
     # Enable the asset pipeline
     #config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-
+    config.assets.paths << Rails.root.join("assets").to_s
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
