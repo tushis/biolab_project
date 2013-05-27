9682bb821a35f3a214e29a194ce4ce7f95ad6ceb
tushis <tushis@mail.ru>
2013-05-28 01:41:55 +0200

    design and network graph

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,4 +42,6 @@
 //= require DataTables-1.9.4/extras/TableTools/media/js/ZeroClipboard
 //= require DataTables-1.9.4/extras/TableTools/media/js/TableTools
 
+//=require jquery.mousewheel.min.js
+//=require jquery.tagsphere.js
 
